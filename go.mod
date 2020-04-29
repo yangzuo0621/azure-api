03452e59ec4e6fd74ad9404bb3ab643663fcd50c
Zuo Yang <zuya@microsoft.com>
2020-04-29 09:24:00 +0800

    use client-go to communicate with k8s

--- go.mod
+++ go.mod
@@ -8,4 +8,13 @@ require (
 	github.com/Azure/go-autorest/autorest/azure/auth v0.4.2
 	github.com/Azure/go-autorest/autorest/to v0.3.0
 	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/googleapis/gnostic v0.4.1 // indirect
+	github.com/imdario/mergo v0.3.9 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	k8s.io/api v0.17.2 // indirect
+	k8s.io/client-go v0.17.0
+	k8s.io/apimachinery v0.17.2
+	k8s.io/utils v0.0.0-20200414100711-2df71ebbae66 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
-	"github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2020-03-01/containerservice"
-	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/yangzuo0621/azure-api/aks"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
-	clusterName             = "zuya-aks-test-002"
+	clusterName             = "zuya-aks-test-001"
 	subscriptionID          = "99a2af06-30ed-4c4f-9f74-2fc72b032fec"
 	tenantID                = "9c0adc1d-423f-4d79-a7c0-352bc5beadce"
 	clientID                = "0615c5a5-5a0e-4a94-a0a6-490f7847da55"
@@ -24,36 +24,33 @@ var (
 
 func main() {
 
-	payload := containerservice.ManagedCluster{
-		Location: &location,
-		ManagedClusterProperties: &containerservice.ManagedClusterProperties{
-			KubernetesVersion:       &kubernetesVersion,
-			EnableRBAC:              to.BoolPtr(true),
-			EnablePodSecurityPolicy: to.BoolPtr(false),
-			// DiskEncryptionSetID:     &myDiskEncryptionSetName,
-			DNSPrefix:     to.StringPtr("zuyatest30503264-aaaa"),
-			AddonProfiles: nil,
-			ServicePrincipalProfile: &containerservice.ManagedClusterServicePrincipalProfile{
-				ClientID: to.StringPtr(clientID),
-				Secret:   to.StringPtr(clientSecret),
-			},
-			AgentPoolProfiles: &[]containerservice.ManagedClusterAgentPoolProfile{
-				{
-					Name:              to.StringPtr("agentpool0"),
-					Count:             to.Int32Ptr(1),
-					VMSize:            "Standard_DS2_v2",
-					MaxCount:          to.Int32Ptr(3),
-					MinCount:          to.Int32Ptr(1),
-					EnableAutoScaling: to.BoolPtr(true),
-					Type:              "VirtualMachineScaleSets",
-					Mode:              containerservice.System,
-				},
-			},
-		},
-	}
-
-	b, _ := json.Marshal(payload)
-	fmt.Println(string(b))
+	// payload := containerservice.ManagedCluster{
+	// 	Location: &location,
+	// 	ManagedClusterProperties: &containerservice.ManagedClusterProperties{
+	// 		KubernetesVersion:       &kubernetesVersion,
+	// 		EnableRBAC:              to.BoolPtr(true),
+	// 		EnablePodSecurityPolicy: to.BoolPtr(false),
+	// 		// DiskEncryptionSetID:     &myDiskEncryptionSetName,
+	// 		DNSPrefix:     to.StringPtr("zuyatest30503264-aaaa"),
+	// 		AddonProfiles: nil,
+	// 		ServicePrincipalProfile: &containerservice.ManagedClusterServicePrincipalProfile{
+	// 			ClientID: to.StringPtr(clientID),
+	// 			Secret:   to.StringPtr(clientSecret),
+	// 		},
+	// 		AgentPoolProfiles: &[]containerservice.ManagedClusterAgentPoolProfile{
+	// 			{
+	// 				Name:              to.StringPtr("agentpool0"),
+	// 				Count:             to.Int32Ptr(1),
+	// 				VMSize:            "Standard_DS2_v2",
+	// 				MaxCount:          to.Int32Ptr(3),
+	// 				MinCount:          to.Int32Ptr(1),
+	// 				EnableAutoScaling: to.BoolPtr(true),
+	// 				Type:              "VirtualMachineScaleSets",
+	// 				Mode:              containerservice.System,
+	// 			},
+	// 		},
+	// 	},
+	// }
 
 	urlParameters := map[string]interface{}{
 		"subscriptionId":    subscriptionID,
@@ -62,7 +59,32 @@ func main() {
 	}
 
 	client := aks.GetRestClient(clientID, clientSecret, tenantID)
-	aks.CreateAKSCluster(client, urlParameters, apiVersion, &payload)
+	// aks.CreateAKSCluster(client, urlParameters, apiVersion, &payload)
 	// aks.DeleteAKSCluster(client, urlParameters, apiVersion)
-	// aks.ListClusterUserCredentials(client, urlParameters, apiVersion)
+	result := aks.ListClusterUserCredentials(client, urlParameters, apiVersion)
+
+	if (len(*result.Kubeconfigs)) <= 0 {
+		panic("")
+	}
+
+	kubeconfig := (*result.Kubeconfigs)[0]
+	clientcmdConfig, err := clientcmd.Load(*kubeconfig.Value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	clientcmd.NewNonInteractiveClientConfig(*clientcmdConfig, "", &clientcmd.ConfigOverrides{}, nil)
+	directClientcmdConfig := clientcmd.NewNonInteractiveClientConfig(*clientcmdConfig, "", &clientcmd.ConfigOverrides{}, nil)
+	clientRestConfig, err := directClientcmdConfig.ClientConfig()
+
+	clientset, err := kubernetes.NewForConfig(clientRestConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
+	v, _ := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
+	nodes := v.Items
+	fmt.Println(len(nodes))
 }
